f5d189f09c7ed4ff916573496408dc27c2333681
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2025-09-30 12:46:07 +0530

    update

---
+++ server.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Define a route for "/"
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
